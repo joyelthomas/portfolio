b3b1bfc465dc33284c3632cdd53e942d3b756714
joyelthomas <88422889+joyelthomas@users.noreply.github.com>
2023-07-28 01:03:10 +0530

    Update portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,31 +1,33 @@
-// Wait for the HTML content to be loaded
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the container element to display images
     var imageContainer = document.getElementById("imageContainer");
 
-    // Array of image filenames (you can fetch this dynamically from the server as well)
-    var imageFilenames = ["render1.jpg", "render2.jpg"];
+    // Fetch image names using Fetch API
+    fetch("assets/images/")
+        .then(response => response.text())
+        .then(text => {
+            // Parse the directory listing (Note: this may not work for all browsers)
+            var imageFilenames = text
+                .split("\n")
+                .filter(filename => filename.trim() !== "")
+                .filter(filename => /\.(jpg|jpeg|png|gif)$/i.test(filename));
 
-    // Loop through the array of image filenames
-    for (var i = 0; i < imageFilenames.length; i++) {
-        // Create an <img> element for each image
-        var imgElement = document.createElement("img");
+            // Generate HTML for each image
+            imageFilenames.forEach(filename => {
+                var imgElement = document.createElement("img");
+                imgElement.src = "assets/images/" + filename;
 
-        // Set the src attribute to the image filename
-        imgElement.src = "assets/images/" + imageFilenames[i];
+                var imageDiv = document.createElement("div");
 
-        // Create a <div> element to wrap each image with a description
-        var imageDiv = document.createElement("div");
+                var description = document.createElement("p");
+                description.textContent = "Project Description for " + filename;
 
-        // Create a <p> element to add a description for each image
-        var description = document.createElement("p");
-        description.textContent = "Project Description for " + imageFilenames[i];
+                imageDiv.appendChild(imgElement);
+                imageDiv.appendChild(description);
 
-        // Append the image and description to the imageDiv
-        imageDiv.appendChild(imgElement);
-        imageDiv.appendChild(description);
-
-        // Append the imageDiv to the main container
-        imageContainer.appendChild(imageDiv);
-    }
+                imageContainer.appendChild(imageDiv);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching image names:", error);
+        });
 });
